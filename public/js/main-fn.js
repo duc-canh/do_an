9ac72db4ac2d480b8f92cecaabbfed491762cc70
duc-canh <84029160+duc-canh@users.noreply.github.com>
2021-09-06 08:59:21 +0700

    test

---
+++ public/js/main-fn.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($){
+    var pos = $("#row-fn").position();//lay vi tri cua menutop cach top x px
+    $(window).scroll(function (){
+        var posScorll = $(document).scrollTop();
+        if(parseInt(posScorll)>parseInt(pos.top)){
+            $("#row-fn").addClass("wow fadeInDown")
+        }else{
+            $("#row-fn").removeClass("wow fadeInDown")
+        }
+    })
+
+})
+//
