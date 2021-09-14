26c5035e981c8d650577a92006e58b043ece66db
thang8989 <84035578+thang8989@users.noreply.github.com>
2021-09-14 17:22:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express(); // tao 1 ung dung tu express module
+var port = process.env.PORT || 4000;
+app.listen(port,function (){
+    console.log("Server is running....");
+});
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.get("/",function (req,res){ // trang chu
+    //res.send("Trang chu");
+    res.render("home")
+});
+app.get("/cart-items",function (req,res){ // trang chu
+    //res.send("Trang chu");
+    res.render("cart-items")
+});
+app.get("/product-cart",function (req,res){ // trang chu
+    //res.send("Trang chu");
+    res.render("product-cart")
+});
+app.get("/products",function (req,res){ // trang chu
+    //res.send("Trang chu");
+    res.render("products")
+});
+app.get("/account",function (req,res) { // trang chu
+    //res.send("Trang chu");
+    res.render("Account")
+});
+app.get("/product-details",function (req,res) { // trang chu
+    //res.send("Trang chu");
+    res.render("product-details")
+});
+app.get("/contact",function (req,res) { // trang chu
+    //res.send("Trang chu");
+    res.render("contact")
+});
+app.get("/about",function (req,res) { // trang chu
+    //res.send("Trang chu");
+    res.render("about")
+});
