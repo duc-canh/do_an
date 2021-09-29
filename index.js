57b46f1b35ce8e9ebbf23caafb98b37744cefdc3
duc-canh <84029160+duc-canh@users.noreply.github.com>
2021-09-29 08:17:06 +0700

    test

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ app.set("view engine","ejs");
 
 app.get("/",function (req,res){ // trang chu
     //res.send("Trang chu");
-    var txt_sql = "select * from eSanPham;";
-    sql.query(txt_sql,function (err,rs){ // callback
-        if(err) res.status(404).send('Not found?');
-        else res.render("home",{
-            hanghoa:rs.recordset
-        });// rows.recordset : 1 array, mỗi element là 1 object từ table
-    })
-    //res.render("home")
+    // var txt_sql = "select * from eSanPham;";
+    // sql.query(txt_sql,function (err,rs){ // callback
+    //     if(err) res.status(404).send('Not found?');
+    //     else res.render("home",{
+    //         hanghoa:rs.recordset
+    //     });// rows.recordset : 1 array, mỗi element là 1 object từ table
+    // })
+    res.render("home")
 });
 app.get("/cart-items",function (req,res){ // trang chu
     //res.send("Trang chu");
@@ -28,31 +28,32 @@ app.get("/product-cart",function (req,res){ // trang chu
 });
 app.get("/products",function (req,res){ // trang chu
     //res.send("Trang chu");
-    var kw = req.query.txt||"";
-    var txt_sql = "select * from eSanPham where TenSP like '%"+kw+"%';";
-    sql.query(txt_sql,function (err,rs){ // callback
-        if(err) res.status(404).send('Not found?');
-        else res.render("products",{
-            hanghoa:rs.recordset
-        });// rows.recordset : 1 array, mỗi element là 1 object từ table
-    })
-    //res.render("products")
+    // var kw = req.query.txt||"";
+    // var txt_sql = "select * from eSanPham where TenSP like '%"+kw+"%';";
+    // sql.query(txt_sql,function (err,rs){ // callback
+    //     if(err) res.status(404).send('Not found?');
+    //     else res.render("products",{
+    //         hanghoa:rs.recordset
+    //     });// rows.recordset : 1 array, mỗi element là 1 object từ table
+    // })
+    res.render("products")
 });
 app.get("/account",function (req,res) { // trang chu
     //res.send("Trang chu");
     res.render("Account")
 });
 app.get("/product-details",function (req,res) { // trang chu
-    var id = req.query.id;
-    var txt_sql = "select * from eSanPham where ID = "+id+";";
-    sql.query(txt_sql,function (err,rs){
-        if(err) res.send(err);
-        else if(rs.recordset.length > 0){
-            res.render("Product-Details",{
-                p:rs.recordset[0]
-            })
-        }else res.status(404).send('Not found?');
-    })
+    // var id = req.query.id;
+    // var txt_sql = "select * from eSanPham where ID = "+id+";";
+    // sql.query(txt_sql,function (err,rs){
+    //     if(err) res.send(err);
+    //     else if(rs.recordset.length > 0){
+    //         res.render("Product-Details",{
+    //             p:rs.recordset[0]
+    //         })
+    //     }else res.status(404).send('Not found?');
+    // })
+    res.render("Product-Details")
 });
 app.get("/contact",function (req,res) { // trang chu
     //res.send("Trang chu");
@@ -66,45 +67,45 @@ app.get("/checkout",function (req,res) { // trang chu
     //res.send("Trang chu");
     res.render("checkout_detail")
 });
-var mssql = require("mssql");
+// var mssql = require("mssql");
+//
+// var ConnectionConfig = {
+//     server: 'DESKTOP-V4TRK5T',
+//     authentication: { type: 'default', options: { userName: 'sa', password: '123a' } },
+//     database:'doann',
+//     options: { encrypt:false }}
+//
+// // ket noi voi database
+// mssql.connect(ConnectionConfig,function (err){
+//     if(err) console.log(err);
+//     else console.log("connected database!");
+// })
+// var sql = new mssql.Request();
 
-var ConnectionConfig = {
-    server: 'DESKTOP-V4TRK5T',
-    authentication: { type: 'default', options: { userName: 'sa', password: '123a' } },
-    database:'doann',
-    options: { encrypt:false }}
+// app.get("/khach-hang",function (req,res){
+//     // can lay danh sach khach hang
+//     var kw = req.query.keyword||"";
+//     var txt_sql = "select * from eSanPham where TenSP like '%"+kw+"%';";
+//     sql.query(txt_sql,function (err,rs){ // callback
+//         if(err) res.status(404).send('Not found?');
+//         else res.render("sanpham",{
+//             hanghoa:rs.recordset
+//         });// rows.recordset : 1 array, mỗi element là 1 object từ table
+//     })
+// });
 
-// ket noi voi database
-mssql.connect(ConnectionConfig,function (err){
-    if(err) console.log(err);
-    else console.log("connected database!");
-})
-var sql = new mssql.Request();
-
-app.get("/khach-hang",function (req,res){
-    // can lay danh sach khach hang
-    var kw = req.query.keyword||"";
-    var txt_sql = "select * from eSanPham where TenSP like '%"+kw+"%';";
-    sql.query(txt_sql,function (err,rs){ // callback
-        if(err) res.status(404).send('Not found?');
-        else res.render("sanpham",{
-            hanghoa:rs.recordset
-        });// rows.recordset : 1 array, mỗi element là 1 object từ table
-    })
-});
-
-app.get("/thuong-hieu",function (req,res) { // trang chu
-    //res.send("Trang chu");
-    var kw = req.query.keyworda||"";
-    var txt_sql = "select * from eSanPham inner join Nhasx on eSanPham.NhasxID = Nhasx.ID where TenNSX like '%"+kw+"%';";
-    sql.query(txt_sql,function (err,rs){ // callback
-        if(err) res.status(404).send('Not found?');
-        else res.render("test",{
-            hanghoa:rs.recordset
-        });// rows.recordset : 1 array, mỗi element là 1 object từ table
-    })
-});
-app.get("/compare",function (req,res) { // trang chu
-    //res.send("Trang chu");
-    res.render("commpare")
-});
+// app.get("/thuong-hieu",function (req,res) { // trang chu
+//     //res.send("Trang chu");
+//     var kw = req.query.keyworda||"";
+//     var txt_sql = "select * from eSanPham inner join Nhasx on eSanPham.NhasxID = Nhasx.ID where TenNSX like '%"+kw+"%';";
+//     sql.query(txt_sql,function (err,rs){ // callback
+//         if(err) res.status(404).send('Not found?');
+//         else res.render("test",{
+//             hanghoa:rs.recordset
+//         });// rows.recordset : 1 array, mỗi element là 1 object từ table
+//     })
+// });
+// app.get("/compare",function (req,res) { // trang chu
+//     //res.send("Trang chu");
+//     res.render("commpare")
+// });
